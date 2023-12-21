82dde99f6ca59d4518d42140acf4bc15133e01ee
HamSangeok <hamsoo159@naver.com>
2023-12-22 01:42:12 +0900

    socketio room

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,164 @@
 import {useEffect, useRef, useState} from 'react';
 
+import { io } from "socket.io-client";
+
 function App() {
-  const [messages, setMessages] = useState([]);
-  const webSocket = useRef(null);
+  const [room, setRoom] = useState("");
+  const [nickname, setNickname] = useState("Anonymous");
+  const [sendMessage, setSendMessage] = useState('');
+  const [enterRoom, setEnterRoom] = useState(false);
+  const [allMessages, setAllMessages] = useState([]);
+  const [publicRooms, setPublicRooms] = useState([]);
+
+  const socketRef = useRef();
+
+  const handleRoomName = (e) => {
+    setRoom(e.target.value);
+  }
+
+  const handleEnterRoom = (e) => {
+    e.preventDefault();
+    setAllMessages([]);
+    socketRef.current.emit("enter_room", {enterRoom: room});
+    console.log(`${socketRef.current.id}님이 ${room}방에 입장하셨습니다.`);
+    setEnterRoom(true);
+  }
+
+  const handleLeaveRoom = (e) => {
+    e.preventDefault();
+    socketRef.current.emit("leave_room", {leaveRoom: room});
+    console.log(`${socketRef.current.id}님이 ${room}방에서 나가셨습니다.`)
+    setEnterRoom(false);
+  }
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    socketRef.current.emit("client_send_message", {room: room, message: sendMessage});
+    setSendMessage("");
+  }
+
+  const handleNickname= (e) => {
+    e.preventDefault();
+    socketRef.current.emit("change_nickname", {nickname: nickname});
+  }
+
+  const onChangeNickname = (e) =>{
+    e.preventDefault();
+    setNickname(e.target.value);
+  }
+
+  const onChangeMessage = (e) =>{
+    e.preventDefault();
+    setSendMessage(e.target.value);
+  }
+
+  useEffect(() => {
+    // Create the socket connection only once when the component mounts
+    socketRef.current = io("http://localhost:3001");
+
+    // Set up event listeners or any other socket-related logic here
+
+    return () => {
+      // Clean up the socket connection when the component is unmounted
+      socketRef.current.disconnect();
+    };
+  }, []); // Empty dependency array ensures this effect runs only once
 
   useEffect(()=>{
-    webSocket.current = new WebSocket(`ws://localhost:3001/`);
-
-    webSocket.current.onopen = ()=>{
-      console.log('WebSocket 연결!');
-    }
-    webSocket.current.onmessage = (message) => {   
-      console.log('event.data', message.data);
-      setMessages((prev) => [...prev, message.data]);
-    };
-
-    setTimeout(() => {
-      if (webSocket.current.readyState === WebSocket.OPEN) {
-        webSocket.current.send("This is a test message from client.");
-        console.log('서버로 메시지 전송');
+    // if(effectForRef.current === false){
+      const fetchWelcome = () => {
+        socketRef.current.on("welcome", (data)=>{
+          console.log(data);
+          setAllMessages((pre)=>[...pre, data]);
+        });
       }
-    }, 10000); // Set the timeout to 10 seconds (10000 milliseconds)
 
-    fetch('http://localhost:3001/')
-    .then((res)=>res.json())
-    .then((res)=>{
-      console.log(res);
-    });
-  },[])
+      const fetchBye = () => {
+        socketRef.current.on("bye", (data)=>{    
+          setAllMessages((pre)=>[...pre, data]);
+        });
+      }
+      
+      const fetchMessage = () => {
+        socketRef.current.on("server_send_message", (data)=>{
+          setAllMessages((pre)=>[...pre, data]);
+        });
+      }
+
+      const fetchPublicRooms = () => {
+        socketRef.current.on("public_rooms", (data)=>{
+          console.log(data);
+          setPublicRooms(data);
+        });
+      }
+
+      fetchWelcome();
+      fetchBye();
+      fetchMessage();
+      fetchPublicRooms(); 
+    // }
+
+    // return () => { effectForRef.current = true };
+  },[socketRef]);
 
   return (
     <div className="App">
+      {
+        enterRoom ? (
+        <div> 
+          <h1>{room}</h1>
+          <ul>
+            {
+              allMessages.map((item, index) => {
+                return <li key={index}>{item}</li>
+              })
+            }
+          </ul>
+          <form>
+            <input 
+              value={sendMessage} 
+              onChange={onChangeMessage} 
+              type="text" 
+              placeholder="message" 
+            />
+            <button onClick={handleSendMessage}>메세지 전송</button>
+
+            <button onClick={handleLeaveRoom}>방 나가기</button>
+          </form>
+          <form>
+            <input 
+              value={nickname} 
+              onChange={onChangeNickname} 
+              type="text" 
+              placeholder="nickname" 
+            />
+            <button onClick={handleNickname}>닉네임 설정</button>
+          </form>
+        </div>
+        ) : (
+          <div>
+            <form>
+              <input 
+                value={room} 
+                onChange={handleRoomName} 
+                type="text" 
+                placeholder='room name' 
+                required
+              />
+              <button onClick={handleEnterRoom}>방 입장</button>
+            </form>
+          </div>
+        )
+      }
+
+      <div>
+        <h1>public room</h1>
+        {
+          publicRooms.map((item, index) => {
+            return <li key={index}>{item.roomName}  {item.roomCount}</li>
+          })
+        }
+      </div>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const http = require('http');
 const app = express();
-const WebSocket = require("ws");
 const cors = require("cors");
+const { Server } = require("socket.io");
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+
+// const io = SocketIo(server);
 
 // cors
 app.use(cors({ origin: [
@@ -12,21 +13,74 @@ app.use(cors({ origin: [
     'https://nomalog.netlify.app'
 ]}));
 
-wss.on("connection", (socket)=>{
-    console.log("클라이언트와 연결 성공");
-    socket.send("서버에서 당신의 접속을 환영하고 있습니다.");
+const io = new Server(server, {
+    cors : {
+        origin : "http://localhost:3000",
+    }
+})
 
-    socket.on("close", ()=>{
-        console.log("클라이언트와 연결 해제");
+const publicRoom = () => {
+    const {
+        sockets : {
+            adapter : {sids, rooms},
+        },
+    
+    } = io;
+
+    console.log(rooms);
+
+    const publicRooms = [];
+    rooms.forEach((_, key)=>{
+        // public room이라면
+        if(sids.get(key) === undefined){
+            const count = countRoom(key);
+            const roomObj = {
+                roomName : key,
+                roomCount : count,
+            }
+            publicRooms.push(roomObj);
+            // publicRooms.push(key);
+        }
     });
 
-    socket.on("message", (message)=>{
-        console.log(message.toString('utf8'));
-    })
-});
+    return publicRooms;
+}
 
-app.get('/', (req, res) => {
-    res.json("Hello world!");
+const countRoom = (roomName) => {
+    return io.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+
+
+io.on("connection", (socket)=>{
+    console.log("클라이언트와 연결 성공");
+    socket.nickname = "Anonymous";
+
+    socket.on("enter_room", (data)=>{
+        console.log(`${socket.nickname}님이 ${data.enterRoom}방에 입장하셨습니다.`)
+        socket.join(data.enterRoom);
+        io.to(data.enterRoom).emit("welcome", `환영합니다. ${socket.nickname}님이 입장하셨습니다.`);
+        // socket.broadcast.emit : 본인을 제외한 모두에게 broadcast
+        io.emit("public_rooms", publicRoom());
+    });
+
+    socket.on("leave_room", (data)=>{
+        console.log(`${socket.nickname}님이 ${data.leaveRoom}방에서 나가셨습니다.`)
+        socket.leave(data.leaveRoom);
+        io.to(data.leaveRoom).emit("bye", `안녕히 가세요. ${socket.nickname}님이 나가셨습니다.`);
+        io.emit("public_rooms", publicRoom());
+    });
+
+    socket.on("client_send_message", (data)=>{
+        console.log(`${socket.nickname}님이 ${data.room}방에 ${data.message}라는 메시지를 보냈습니다.`)
+        io.to(data.room).emit("server_send_message", `${socket.nickname} : ${data.message}`);
+    });
+
+    socket.on("change_nickname", (data)=>{
+        console.log(`${socket.nickname}님이 ${data.nickname}으로 닉네임을 변경하셨습니다.`)
+        socket.nickname = data.nickname;
+        console.log(`${socket.nickname}님이 ${socket.nickname}으로 닉네임을 변경이 완료되었습니다.`)
+    });
 });
 
 module.exports = {
